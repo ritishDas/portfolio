537b7d20ce090995b1eaa5b7e4208bef03391945
ritishDas <ritishdas116@outlook.com>
2025-09-11 13:27:03 +0530

    styling

--- script.js
+++ script.js
@@ -1,35 +1,58 @@
 {
   const skillsparent = fetchElement("#skills");
 
-  let newElement;
-  newElement = gardener({ t:'div',
-    cn:['flex', 'justify-around', 'm-10', 'p-5', 'rounded-md', 'flex-wrap', 'space-y-5'] 
+  let newElement = gardener({
+    t:'div',
+    cn:[
+      'flex', 'justify-center', 'gap-8',
+      'm-10', 'p-5',
+      'flex-wrap'
+    ] 
   });
 
   let skills = db.skills;
-  console.log(skills);
-  for(let key in skills) {
-
-    let tempElement = gardener({ t:'div',
-      cn:['w-3/10', 'p-3', 'bg-white', 'rounded-md'] 
+  for (let key in skills) {
+    let tempElement = gardener({
+      t:'div',
+      cn:[
+        'w-full', 'sm:w-[45%]', 'lg:w-[30%]',
+        'p-5',
+        'bg-background-800',
+        'rounded-xl',
+        'shadow-md',
+        'transition', 'hover:shadow-lg'
+      ] 
     });
 
-
-    let skillList =  gardener({ t:'div',
-      cn:['flex', 'flex-wrap', 'gap-2','p-2', 'mt-2'] 
+    let skillList = gardener({
+      t:'div',
+      cn:[
+        'flex', 'flex-wrap',
+        'gap-2', 'mt-3'
+      ] 
     });
 
     skills[key].forEach(skill => {
       appendElement(skillList, gardener({ 
         t:'span', 
         txt: skill, 
-        cn:['p-0.5', 'bg-gray-300', 'rounded-lg'] 
+        cn:[
+          'px-2', 'py-1',
+          'bg-primary-600/30',
+          'text-primary-400',
+          'rounded-md',
+          'text-sm',
+          'font-medium'
+        ] 
       }))
     });
 
     appendElement(tempElement, gardener({ 
       t:'h3', 
-      cn:['font-bold', 'text-center', 'text-lg'], 
+      cn:[
+        'font-bold', 'text-center',
+        'text-lg', 'text-primary-300'
+      ], 
       txt: key 
     }));
 
@@ -39,48 +62,69 @@
   }
 
   replaceElement(skillsparent, newElement);
-
 }
 
+// === Featured Section ===
 const featuredParent = fetchElement("#featured");
 const container = gardener({
   t:'div',
-  cn:['flex', 'justify-around']
+  cn:[
+    'grid',
+    'gap-8',
+    'sm:grid-cols-2',
+    'lg:grid-cols-3',
+    'px-6'
+  ]
 });
 
-db.featured.forEach( featured => {
+db.featured.forEach(featured => {
   const card = gardener({
     t:'div',
-    cn:['w-3/10', 'border', 'bg-white'],
+    cn:[
+      'bg-background-800',
+      'rounded-xl',
+      'overflow-hidden',
+      'shadow-md',
+      'transition',
+      'hover:shadow-lg'
+    ],
     children:[
       {
         t:'img',
-        cn:['w-full', 'h-80', 'object-cover'],
+        cn:['w-full', 'h-52', 'object-cover'],
         attr:{
-          src:featured.img,
-          alt:featured.title
+          src: featured.img,
+          alt: featured.title
         }
       },
       {
         t:'div',
-        cn:['p-4'],
+        cn:['p-5'],
         children:[
           {
             t:'h3',
-            cn:['font-bold', 'text-lg', 'mt-2'],
-            txt:featured.title
+            cn:['font-bold', 'text-xl', 'text-primary-300'],
+            txt: featured.title
           },
           {
             t:'p',
-            cn:['mt-1', 'text-sm'],
-            txt:featured.description
+            cn:['mt-2', 'text-sm', 'text-gray-700'],
+            txt: featured.description
           },
           {
             t:'a',
-            cn:['p-2', 'border', 'hover:bg-black', 'hover:text-white', 'w-full', 'block', 'text-center', 'mt-4', 'rounded-md'],
-            attr:{
-              href:featured.link
-            },
+            cn:[
+              'inline-block',
+              'mt-4',
+              'px-4', 'py-2',
+              'rounded-md',
+              'bg-primary-600',
+              'text-text-100',
+              'font-medium',
+              'hover:bg-primary-700',
+              'transition'
+            ],
+            attr:{ href: featured.link },
             txt:"Learn More"
           }
         ]
@@ -89,6 +133,7 @@ db.featured.forEach( featured => {
   });
 
   appendElement(container, card);
-})
+});
 
 replaceElement(featuredParent, container);
+
